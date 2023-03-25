65afeabc99ea7fc85a13dc7f40eb0931c193401b
Lois Omotara <lo2173@princeton.edu>
2023-03-25 12:46:56 -0400

    search results

--- reg.py
+++ reg.py
@@ -1,6 +1,30 @@
 #-----------------------------------------------------------------------
 # Author: Lois I Omotara
 # reg.py
+# []need to ask how to get cookies: set cookie
 #-----------------------------------------------------------------------
 import html
 import flask
+import databasesearch as ds
+import classsearch as cs
+import line
+#-----------------------------------------------------------------------
+app = flask.Flask(__name__)
+#-----------------------------------------------------------------------
+
+
+@app.route('/', methods =['GET'])
+@app.route('/searchresults',methods=['GET'])
+def search_results():
+    dept = flask.request.args.get('dept')
+    num = flask.request.args.get('number')
+    area = flask.request.args.get('area')
+    title = flask.request.args.get('title')
+    search = ds.DatabaseSearch()
+    rawsearch = search.fullsearch(idept=dept,icoursenum=num,
+    iarea=area, ititle=title)
+    course_results = []
+    for row in rawsearch:
+        course_results.append(line.LineParser(row))
+    html_code = flask.render_template('searchresults.html')
+#@app.route('/classresult',methods=['GET'])
