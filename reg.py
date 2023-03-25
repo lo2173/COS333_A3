00f64c1f3ec49de00a7c1771457178f08f24a839
Lois Omotara <lo2173@princeton.edu>
2023-03-25 14:31:56 -0400

    finished stateful updates of stateform

--- reg.py
+++ reg.py
@@ -16,6 +16,10 @@ app = flask.Flask(__name__)
 @app.route('/', methods =['GET'])
 @app.route('/searchresults',methods=['GET'])
 def search_results():
+    #prev_dept= flask.request.cookies.get('deptcookie')
+    #prev_num = flask.request.cookies.get('numcookie')
+    #prev_area = flask.request.cookies.get('areacookie')
+    #prev_title = flask.request.cookies.get('titlecookie')
     dept = flask.request.args.get('dept')
     num = flask.request.args.get('number')
     area = flask.request.args.get('area')
@@ -26,7 +30,28 @@ def search_results():
     course_results_ = []
     for row in rawsearch:
         course_results_.append(line.LineParser(row))
-    html_code = flask.render_template('searchresults.html',course_results = course_results_)
+    if dept is None:
+        dept = ''
+    if num is None:
+        num = ''
+    if area is None:
+        area = ''
+    if title is None:
+        title = ''
+    html_code = flask.render_template('searchresults.html',
+        course_results = course_results_,
+        dept = dept,
+        num = num,
+        area = area,
+        title = title)
     response = flask.make_response(html_code)
+    #if dept is not None:
+     #   response.set_cookie('deptcookie',dept)
+    #if num is not None:
+      #  response.set_cookie('numcookie',num)
+    #if area is not None:
+     #   response.set_cookie('areacookie',area)
+    #if title is not None:
+    #    response.set_cookie('titlecookie',title)
     return response
 #@app.route('/classresult',methods=['GET'])
